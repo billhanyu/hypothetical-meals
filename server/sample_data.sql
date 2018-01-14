8f60a3b238f64c3bc7b120042834476242da6df7
Bill Yu <han.yu@duke.edu>
2018-01-14 18:05:41 -0500

    db + first api

---
+++ server/sample_data.sql
@@ -0,0 +1,7 @@
+insert into users values(1, 'Bill', 'hash', 'salt', 'admin');
+insert into users values(2, 'Eric', 'hash', 'salt', 'noob');
+insert into vendors values(1, 'Duke', 'contact', 'code');
+insert into storages values(1, 'freezer', 10);
+insert into ingredients values(1, 'poop', 'sack', 1, 10, 1);
+insert into inventories values(1, 1, 10);
+insert into logs (id, user_id, ingredient_id, quantity) values (1, 1, 1, 18);
