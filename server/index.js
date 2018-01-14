4a77dd15537311ef9becd5678e04dfaac766108c
Bill Yu <han.yu@duke.edu>
2018-01-14 09:15:17 -0500

    set up api server

--- server/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-const server = require('http').createServer(app);
-
-server.listen(3000, () => {
-  console.log('Node app start at port 1717');
-});
