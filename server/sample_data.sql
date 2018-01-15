2444651dce7e3833377bd72dd97e3314f32b70e3
Bill Yu <han.yu@duke.edu>
2018-01-14 19:48:43 -0500

    first test

--- server/sample_data.sql
+++ server/sample_data.sql
@@ -1,7 +1,9 @@
-insert into users values(1, 'Bill', 'hash', 'salt', 'admin');
-insert into users values(2, 'Eric', 'hash', 'salt', 'noob');
-insert into vendors values(1, 'Duke', 'contact', 'code');
-insert into storages values(1, 'freezer', 10);
-insert into ingredients values(1, 'poop', 'sack', 1, 10, 1);
-insert into inventories values(1, 1, 10);
-insert into logs (id, user_id, ingredient_id, quantity) values (1, 1, 1, 18);
+INSERT INTO Users VALUES(1, 'Bill', 'hash', 'salt', 'admin');
+INSERT INTO Users VALUES(2, 'Eric', 'hash', 'salt', 'noob');
+INSERT INTO Vendors VALUES(1, 'Duke', 'contact', 'code');
+INSERT INTO Storages VALUES(1, 'freezer', 10);
+INSERT INTO Ingredients VALUES(1, 'poop', 'sack', 1, 10, 1);
+INSERT INTO Ingredients VALUES(2, 'beans', 'pail', 1, 20, 1);
+INSERT INTO Inventories VALUES(1, 1, 10);
+INSERT INTO Inventories VALUES(2, 2, 5);
+INSERT INTO Logs (id, user_id, ingredient_id, quantity) VALUES (1, 1, 1, 18);
