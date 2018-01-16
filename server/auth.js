713a77762aac524a3272b5f7eab2281fddc86719
Eric Song <ess42@duke.edu>
2018-01-16 10:14:51 -0500

    Implemented eslint formatting

--- server/auth.js
+++ server/auth.js
@@ -6,25 +6,25 @@ try {
   config = require('./config.example');
 }
 
-function getTokenFromHeader(req){
+function getTokenFromHeader(req) {
   if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Token') {
     return req.headers.authorization.split(' ')[1];
   }
   return null;
 }
 
-var auth = {
+let auth = {
   required: jwt({
     secret: config.secret,
     userProperty: 'payload',
-    getToken: getTokenFromHeader
+    getToken: getTokenFromHeader,
   }),
   optional: jwt({
     secret: config.secret,
     userProperty: 'payload',
     credentialsRequired: false,
-    getToken: getTokenFromHeader
-  })
+    getToken: getTokenFromHeader,
+  }),
 };
 
 module.exports = auth;
