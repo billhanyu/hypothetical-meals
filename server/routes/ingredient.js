5e882929dad33ab0e1ea2e6e92502caacd23deca
Bill Yu <han.yu@duke.edu>
2018-01-17 13:59:00 -0500

    get apis

--- server/routes/ingredient.js
+++ server/routes/ingredient.js
@@ -1,4 +1,34 @@
+export function view(req, res, next) {
+  connection.query('SELECT * FROM Ingredients', (error, results, fields) => {
+    if (error) {
+      console.error(error);
+      return res.status(500).send('Database error');
+    }
+    return res.status(200).send(results);
+  });
+}
+
+/* request body format:
+ * request.body.ingredients = [
+ *   {
+ *     'name': 'p',
+ *     'package_type': 'sail',
+ *     'storage_id': 1,
+ *     'price': 100,
+ *     'vendor_id': 1,
+ *   }
+ *   "inventory_id2": new_quantity2
+ * ]
+ * example:
+ * {
+ *   "1": 123,
+ *   "2": 456
+ * }
+ * This changes ingredient 1's num_packages to 123 and 2's num_packages to 456
+ */
 export function addIngredient(req, res, next) {
+  // TODO: add auth
+
   res.status(501).send('todo');
 }
 
--- server/routes/vendor.js
+++ server/routes/vendor.js
@@ -1,13 +1,9 @@
 export function view(req, res, next) {
-  connection.query('SELECT * FROM Storages', (error, results, fields) => {
+  connection.query('SELECT * FROM Vendors', (error, results, fields) => {
     if (error) {
       console.error(error);
       return res.status(500).send('Database error');
     }
     return res.status(200).send(results);
   });
-}
-
-export function changeStorage(req, res, next) {
-  res.status(501).send('todo');
 }
