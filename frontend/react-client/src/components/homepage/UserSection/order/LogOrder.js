96f05c6adc32f32304b22dccf6eb23f9ed0660bd
Bill Yu <han.yu@duke.edu>
2018-02-02 17:53:48 -0500

    inventory filter display

--- frontend/react-client/src/components/homepage/UserSection/order/LogOrder.js
+++ frontend/react-client/src/components/homepage/UserSection/order/LogOrder.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import OrderItem from '../Components/OrderItem';
+import OrderItem from './OrderItem';
 
 class LogOrder extends Component {
   constructor(props) {
