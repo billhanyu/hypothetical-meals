aba5a8c1f4b54ac5486218125e8c6caebffaace9
Bill Yu <han.yu@duke.edu>
2018-02-03 12:12:14 -0500

    log order ui updates

--- frontend/react-client/src/components/homepage/UserSection/order/OrderItem.js
+++ frontend/react-client/src/components/homepage/UserSection/order/OrderItem.js
@@ -4,62 +4,75 @@ import axios from 'axios';
 class OrderItem extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      vendorIngredients: []
-    };
     this.selectedIngredient = this.selectedIngredient.bind(this);
+    this.remove = this.remove.bind(this);
   }
 
   selectedIngredient(event) {
-    const ingredientId = event.target.value;
-    let vendorIngredients = [];
-    axios.get(`/vendoringredients/${ingredientId}`, {
+    const ingredientName = event.target.value;
+    axios.get('/ingredients-available', {
       headers: { Authorization: "Token " + this.props.token }
     })
-      .then(response => {
-        vendorIngredients = response.data;
-        return axios.get(`/vendors`, {
+    .then(response => {
+      let ingredientId;
+      for (let ingredient of response.data) {
+        if (ingredient.name == ingredientName) {
+          ingredientId = ingredient.id;
+          break;
+        }
+      }
+      if (ingredientId) {
+        return axios.get(`/vendoringredients/${ingredientId}`, {
           headers: { Authorization: "Token " + this.props.token }
         });
-      })
-      .then(response => {
-        const vendorData = response.data;
-        for (let vendorIngredient of vendorIngredients) {
-          for (let data of vendorData) {
-            if (vendorIngredient.vendor_id == data.id) {
-              vendorIngredient.vendorName = data.name;
-              break;
-            }
-          }
-        }
-        this.setState({
-          vendorIngredients
-        });
-      })
-      .catch(err => {
-      });
+      }
+    })
+    .then(response => {
+      this.props.fillVendorIngredients(response.data, this.props.idx);
+    })
+    .catch(err => {
+    });
+  }
+
+  remove() {
+    this.props.removeOrderItem(this.props.idx);
   }
 
   render() {
     return (
       <div className="orderitem">
-        <select className="inputitem" onChange={e => this.selectedIngredient(e)}>
+        <div
+          className="glyphicon glyphicon-remove DeleteOrderColumn DeleteOrderItem"
+          aria-hidden="true"
+          onClick={this.remove}
+        />
+        <input
+          className="OrderColumn inputitem"
+          onChange={e => { 
+            this.selectedIngredient(e);
+            this.props.onChangeIngredientName(e, this.props.idx);
+          }}
+          value={this.props.data.ingredientName}
+        />
+        <select
+          className="OrderColumn inputitem"
+          onChange={e => this.props.selectedVendorIngredient(e, this.props.idx)}
+          value={this.props.data.vendor_ingredient_id}
+        >
           <option value="" disabled selected style={{display: "none"}}>None</option>
           {
-            this.props.ingredients.map((element, key) => {
-              return <option value={element.id} key={key}>{element.name}</option>;
+            this.props.data.vendoringredients.map((element, key) => {
+              return <option value={element.id} key={key}>{element.vendor_name}</option>;
             })
           }
         </select>
-        <select className="inputitem" onChange={e => this.props.selectedVendorIngredient(e, this.props.idx)}>
-          <option value="" disabled selected style={{display: "none"}}>None</option>
-          {
-            this.state.vendorIngredients.map((element, key) => {
-              return <option value={element.id} key={key}>{element.vendorName}</option>;
-            })
-          }
-        </select>
-        <input className="inputitem" type="text" name="quantity" onChange={e => this.props.changedQuantity(e, this.props.idx)} />
+        <input
+          className="OrderColumn inputitem"
+          type="text"
+          name="quantity"
+          onChange={e => this.props.changedQuantity(e, this.props.idx)}
+          value={this.props.data.quantity}
+        />
       </div>
     );
   }
