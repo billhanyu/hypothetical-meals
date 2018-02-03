964def4574083d2e4c40c6fc2242cebe4b993c7a
Christine Zhou <zhou.l.chistine@gmail.com>
2018-02-02 23:21:10 -0500

    Still testing.

--- server/routes/order.js
+++ server/routes/order.js
@@ -1,6 +1,9 @@
-import modifyInventoryQuantitiesPromise from './inventory';
+import { modifyInventoryQuantitiesPromise } from './inventory';
+import { addEntry } from './log';
 import { createError, handleError } from './common/customError';
 import { getWeight } from './common/packageUtilies';
+import { checkStoragePromise } from './common/storageUtilities';
+import success from './common/success';
 
 /* request body format:
  * request.body.order = {
@@ -17,12 +20,12 @@ import { getWeight } from './common/packageUtilies';
  * 3. logs and spending logs
  */
 export function placeOrder(req, res, next) {
-    orderHelper(req.body.orders, res, next);
+    orderHelper(req.body.orders, req, res, next);
 }
 
 function orderHelper(orders, req, res, next) {
-    connection.query(`SELECT VendorsIngredients.id, VendorsIngredients.ingredient_id, VendorsIngredients.package_type, Ingredients.storage_type 
-    FROM VendorsIngredients JOIN Ingredients ON VendorsIngredients.ingredient_id = Ingredients.id WHERE VendorsIngredients.id IN ${Object.keys(orders).join(', ')}`)
+    connection.query(`SELECT VendorsIngredients.id, VendorsIngredients.ingredient_id, VendorsIngredients.package_type, Ingredients.storage_id 
+    FROM VendorsIngredients JOIN Ingredients ON VendorsIngredients.ingredient_id = Ingredients.id WHERE VendorsIngredients.id IN (${Object.keys(orders).join(', ')})`)
     .then((results) => {
         if (results.length < Object.keys(orders).length) {
             throw createError('Some id not in Vendor Ingredients');
@@ -32,15 +35,15 @@ function orderHelper(orders, req, res, next) {
         let newIngredientCases = [];
         let ingredientsMap = {};
         for (let result of results) {
-            ingredientsMap[result[ingredient_id]] = {
-                'package_type': result[package_type],
+            ingredientsMap[result['ingredient_id']] = {
+                'package_type': result['package_type'],
                 'quantity': orders[result.id],
-                'storage_id': result[storage_id],
-                'vendor_ingedient_id': result[id],
+                'storage_id': result['storage_id'],
+                'vendor_ingredient_id': result['id'],
             };
         }
         let updateIngredients = {};
-        connection.query(`SELECT id, ingredient_id FROM Inventories WHERE ingredient_id IN ${ingredientIds.join(', ')}`)
+        connection.query(`SELECT id, ingredient_id FROM Inventories WHERE ingredient_id IN (${ingredientIds.join(', ')})`)
         .then((inventoryResults) => {
             inventoryResults.forEach(x => {
                 updateIngredients[x.id] = orders[x.ingredient_id];
@@ -51,16 +54,35 @@ function orderHelper(orders, req, res, next) {
                     requestedCapacities[ingredientsMap[ingredientId]['storage_id']] += ingredientsMap[ingredientId]['quantity'] * getWeight(ingredientsMap[ingredientId]['package_type']);
                 }
             }
-            return modifyInventoryQuantitiesPromise(updateIngredients);
+            if (Object.keys(updateIngredients).length > 0) {
+                modifyInventoryQuantitiesPromise(updateIngredients)
+                .catch((err) => {
+                    throw err;
+                });
+            }
+        })
+        .catch((err) => {
+            throw err;
         })
         .then(() => {
             checkStoragePromise(requestedCapacities)
             .then(() => {
-                return connection.query(`INSERT INTO Inventories (ingredient_id, package_type, quantity) VALUES ${newIngredientCases.join(', ')}`);
+                if (newIngredientCases.length > 0) {
+                    return connection.query(`INSERT INTO Inventories (ingredient_id, package_type, quantity) VALUES ${newIngredientCases.join(' ')}`);
+                }
+            })
+            .catch((err) => {
+                throw err;
             })
             .then(() => {
                 let logReq = Object.values(ingredientsMap);
-                return addEntry(logReq, req.payload.id);
+                addEntry(logReq, req.payload.id)
+                .then(() => {
+                    success(res);
+                })
+                .catch((err) => {
+                    throw err;
+                });
             })
             .catch((err) => {
                 throw err;
