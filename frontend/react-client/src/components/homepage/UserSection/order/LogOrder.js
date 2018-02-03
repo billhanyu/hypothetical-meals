76a48ef59740968fea767937ec5c6bdc409c0daa
Bill Yu <han.yu@duke.edu>
2018-02-03 12:12:14 -0500

    log order ui updates

--- frontend/react-client/src/components/homepage/UserSection/order/LogOrder.js
+++ frontend/react-client/src/components/homepage/UserSection/order/LogOrder.js
@@ -11,35 +11,64 @@ class LogOrder extends Component {
       completed: false,
     };
     this.addOrderItem = this.addOrderItem.bind(this);
+    this.onChangeIngredientName = this.onChangeIngredientName.bind(this);
     this.selectedVendorIngredient = this.selectedVendorIngredient.bind(this);
     this.changedQuantity = this.changedQuantity.bind(this);
     this.commitOrder = this.commitOrder.bind(this);
+    this.removeOrderItem = this.removeOrderItem.bind(this);
+    this.fillVendorIngredients = this.fillVendorIngredients.bind(this);
   }
 
   addOrderItem() {
     const newOrders = this.state.orders.slice();
     newOrders.push({
+      ingredientName: '',
       vendor_ingredient_id: null,
       quantity: 0,
+      vendoringredients: [],
     });
     this.setState({
       orders: newOrders
+    });
+  }
+
+  removeOrderItem(component_id) {
+    const newOrders = this.state.orders.slice();
+    newOrders.splice(component_id, 1);
+    this.setState({
+      orders: newOrders
+    });
+  }
+
+  onChangeIngredientName(event, idx) {
+    const orders = this.state.orders.slice();
+    orders[idx].ingredientName = event.target.value;
+    this.setState({
+      orders
     });
   }
 
   selectedVendorIngredient(event, idx) {
-    const newOrders = this.state.orders.slice();
-    newOrders[idx].vendor_ingredient_id = event.target.value;
+    const orders = this.state.orders.slice();
+    orders[idx].vendor_ingredient_id = event.target.value;
     this.setState({
-      orders: newOrders
+      orders
     });
   }
 
   changedQuantity(event, idx) {
-    const newOrders = this.state.orders.slice();
-    newOrders[idx].quantity = parseInt(event.target.value);
+    const orders = this.state.orders.slice();
+    orders[idx].quantity = parseInt(event.target.value);
     this.setState({
-      orders: newOrders
+      orders
+    });
+  }
+
+  fillVendorIngredients(vendoringredients, idx) {
+    const orders = this.state.orders.slice();
+    orders[idx].vendoringredients = vendoringredients;
+    this.setState({
+      orders
     });
   }
 
@@ -78,10 +107,11 @@ class LogOrder extends Component {
       <div>
         {!this.state.completed &&
           <div>
-            <div>
-              <span className="OrderColumn">Ingredient</span>
-              <span className="OrderColumn">Vendor</span>
-              <span className="OrderColumn">Quantity</span>
+            <div className="orderitem">
+              <div className="DeleteOrderColumn" />
+              <span className="OrderColumn OrderColumnHeader">Ingredient</span>
+              <span className="OrderColumn OrderColumnHeader">Vendor</span>
+              <span className="OrderColumn OrderColumnHeader">Quantity</span>
             </div>
             {
               this.state.orders.map((item, key) => 
@@ -91,14 +121,17 @@ class LogOrder extends Component {
                   token={this.props.token}
                   ingredients={this.state.ingredients}
                   data={item}
+                  onChangeIngredientName={this.onChangeIngredientName}
                   selectedVendorIngredient={this.selectedVendorIngredient}
                   changedQuantity={this.changedQuantity}
+                  removeOrderItem={this.removeOrderItem}
+                  fillVendorIngredients={this.fillVendorIngredients}
                 />
               )
             }
-            <div className="OrderButtons">
+            <div className="OrderButtons orderitem">
               <button className="AdminButton" onClick={this.addOrderItem}>Add Another</button>
-              <button className="AdminButton btn btn-primary" onClick={this.commitOrder}>Order</button>
+              <button className="btn btn-primary AdminButton" onClick={this.commitOrder}>Order</button>
             </div>
           </div>
         }
