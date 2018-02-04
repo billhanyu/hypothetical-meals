22cd767618ef9bc3aeb1bef80904e18dcf5ce802
Christine Zhou <zhou.l.christine@gmail.com>
2018-02-04 00:06:22 -0500

    Fixing tests after merge conflicts.

--- test/log.js
+++ test/log.js
@@ -126,7 +126,7 @@ describe('Log', () => {
 
         const newSpendingLogs = alasql('SELECT * FROM SpendingLogs');
         assert.strictEqual(newSpendingLogs[0]['total'], 5100, 'Total spent for ingredient 1.');
-        assert.strictEqual(newSpendingLogs[0]['total_weight'], 0, 'Total weight for ingredient 1.');
+        assert.strictEqual(newSpendingLogs[0]['total_weight'], 1000, 'Total weight for ingredient 1.');
         assert.strictEqual(newSpendingLogs[0]['ingredient_id'], 1, 'Ingredient id for spending log 1.');
         assert.strictEqual(newSpendingLogs[0]['consumed'], 50, 'Quantity consumed for ingredient 1.');
         assert.strictEqual(newSpendingLogs[1]['total'], 200, 'Total spent for ingredient 2.');
