24f8141876bcbddd7e37a2d8875a422cc562c39e
Brian Zhou <brian.zhou@duke.edu>
2018-02-05 21:07:50 -0500

    Bug fixes and error polishes

--- frontend/react-client/src/components/homepage/AdminSection/Storage/EditStorageCapacity.js
+++ frontend/react-client/src/components/homepage/AdminSection/Storage/EditStorageCapacity.js
@@ -11,10 +11,15 @@ class EditVendor extends Component {
   constructor(props){
     super(props);
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleComboBoxChange = this.handleComboBoxChange.bind(this);
     this.handleSubmitButtonClick = this.handleSubmitButtonClick.bind(this);
     this.state = {
       size: 0,
       storage: 'Frozen',
+      errorMessage: null,
+      frozen_storage: 0,
+      refrigerated_storage: 0,
+      room_temp_storage: 0,
       errorMessage: null,
     };
   }
@@ -23,10 +28,48 @@ class EditVendor extends Component {
     1. Token (String)
   */
 
+  componentDidMount(){
+    const self = this;
+    axios.get("/storages", {
+      headers: { Authorization: "Token " + this.props.token }
+    })
+    .then(function (response) {
+      self.setState({
+        frozen_storage: response.data[0].capacity,
+        refrigerated_storage: response.data[1].capacity,
+        room_temp_storage: response.data[2].capacity,
+        size: response.data[0].capacity,
+        errorMessage: null,
+      });
+    })
+    .catch(error => {
+      self.setState({
+        errorMessage: error.response.data,
+      });
+    });
+  }
+
   handleInputChange(fieldName, event) {
     const newState = this.state;
-    this.state[fieldName] = event.target.value;
+    newState[fieldName] = event.target.value;
     this.setState(newState);
+  }
+
+  handleComboBoxChange(fieldName, event) {
+    let newPriceForStorage = 0;
+    const storage = event.target.value;
+    if(storage == "Frozen") {
+      newPriceForStorage = this.state.frozen_storage;
+    }
+    else if (storage == 'Refrigerated') {
+      newPriceForStorage = this.state.refrigerated_storage;
+    }
+    else if (storage == 'Room Temperature') {
+      newPriceForStorage = this.state.room_temp_storage;
+    }
+    this.setState({
+      size: newPriceForStorage,
+    });
   }
 
   handleSubmitButtonClick() {
@@ -51,13 +94,13 @@ class EditVendor extends Component {
     .then(function (response) {
       self.setState({
         hasUpdated: true,
+        errorMessage: null,
       });
     })
     .catch(error => {
       self.setState({
-        errorMessage: error.data
+        errorMessage: error.response.data
       });
-      console.log(error.response);
     });
   }
 
@@ -68,7 +111,7 @@ class EditVendor extends Component {
 
           <RegistrationInput inputClass="RegistrationInput" placeholderText="Size" onChange={this.handleInputChange} id="size" value={this.state.size}/>
           <div className="RegistrationInfoText">* Max Capacity</div>
-          <VendorComboBox id="storage" Options={["Frozen","Refrigerated","Room Temperature"]} onChange={this.handleInputChange}/>
+          <VendorComboBox id="storage" Options={["Frozen","Refrigerated","Room Temperature"]} onChange={this.handleComboBoxChange}/>
           <div className="RegistrationInfoText">* Storage Type </div>
 
           <div className="RegistrationSubmitButton" onClick={this.handleSubmitButtonClick}>EDIT STORAGE SIZE</div>
