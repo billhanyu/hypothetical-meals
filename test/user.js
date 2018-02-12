6568e7abd0dbdce80fa4b92f03d12d171591e903
Bill Yu <han.yu@duke.edu>
2018-02-12 13:10:31 -0500

    some tests for user login

--- test/user.js
+++ test/user.js
@@ -3,6 +3,124 @@ const assert = require('chai').assert;
 const testTokens = require('./testTokens');
 
 describe('User', () => {
+  describe('#loginOauth()', () => {
+    beforeEach(() => {
+      alasql('SOURCE "./server/create_database.sql"');
+      alasql('SOURCE "./server/sample_data.sql"');
+    });
+
+    it('should deny invalid input object', (done) => {
+      chai.request(server)
+        .post('/users/login/oauth')
+        .send({
+          'content': {
+            'netid': 'hy103',
+            'name': 'Bill Yu',
+          },
+        })
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        });
+    });
+
+    it('should deny invalid input object info', (done) => {
+      chai.request(server)
+        .post('/users/login/oauth')
+        .send({
+          'info': {
+            'name': 'Bill Yu', // no netid
+          },
+        })
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        });
+    });
+
+    it('should add new netid users to table', (done) => {
+      const oldLength = alasql('SELECT id FROM Users').length;
+      chai.request(server)
+        .post('/users/login/oauth')
+        .send({
+          'info': {
+            'netid': 'hy103',
+            'name': 'Bill Yu',
+          },
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          const newLength = alasql('SELECT id FROM Users').length;
+          assert.strictEqual(oldLength + 1, newLength, 'Add new user to table');
+          const addedUser = alasql('SELECT name, oauth FROM Users WHERE netid = \'hy103\'');
+          assert.strictEqual(addedUser[0].name, 'Bill Yu', 'Bill Yu registered to database');
+          assert.strictEqual(addedUser[0].oauth, 1, 'User is oauth');
+          assert.strictEqual(addedUser[0].user_group, 'noob', 'Default to noob user');
+          done();
+        });
+    });
+
+    it('should not add existing netid user to table', (done) => {
+      const oldLength = alasql('SELECT id FROM Users').length;
+      chai.request(server)
+        .post('/users/login/oauth')
+        .send({
+          'info': {
+            'netid': 'eri101',
+            'name': 'Eric Song',
+          },
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          const newLength = alasql('SELECT id FROM Users').length;
+          assert.strictEqual(oldLength, newLength, 'Eric existing, so no new user');
+          done();
+        });
+    });
+
+    it('should return info and token for oauth new user log in', (done) => {
+      chai.request(server)
+        .post('/users/login/oauth')
+        .send({
+          'info': {
+            'netid': 'hy103',
+            'name': 'Bill Yu',
+          },
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          const user = res.body.user;
+          assert.strictEqual(user.name, 'Bill Yu', 'Return info name');
+          assert.strictEqual(user.username, 'hy103', 'Return info username');
+          assert.strictEqual(user.user_group, 'noob', 'Return info user_group');
+          assert.strictEqual(user.oauth, 1, 'Return info oauth');
+          assert(user.token && user.token.length > 0, 'Token generated for oauth new user');
+          done();
+        });
+    });
+
+    it('should return info and token for oauth old user log in', (done) => {
+      chai.request(server)
+        .post('/users/login/oauth')
+        .send({
+          'info': {
+            'netid': 'eri101',
+            'name': 'Eric Song',
+          },
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          const user = res.body.user;
+          assert.strictEqual(user.name, 'Eric Song', 'Return info name');
+          assert.strictEqual(user.username, 'eri101', 'Return info username');
+          assert.strictEqual(user.user_group, 'noob', 'Return info user_group');
+          assert.strictEqual(user.oauth, 1, 'Return info oauth');
+          assert(user.token && user.token.length > 0, 'Token generated for oauth new user');
+          done();
+        });
+    });
+  });
+
   describe('#modifyUsers()', () => {
     beforeEach(() => {
       alasql('SOURCE "./server/create_database.sql"');
@@ -26,6 +144,7 @@ describe('User', () => {
           const newAdmin = users[0];
           assert.strictEqual(newAdmin.id, 5, 'New admin ID.');
           assert.strictEqual(newAdmin.username, 'admin1', 'New admin username.');
+          assert.strictEqual(newAdmin.oauth, 0, 'Not OAuth user');
           assert.strictEqual(newAdmin.name, 'mike wazowski', 'New admin name.');
           assert.strictEqual(newAdmin.user_group, 'admin', 'New admin user group.');
           done();
@@ -67,6 +186,7 @@ describe('User', () => {
           const newNoob = users[0];
           assert.strictEqual(newNoob.id, 5, 'New noob ID.');
           assert.strictEqual(newNoob.username, 'noob1', 'New noob username.');
+          assert.strictEqual(newNoob.oauth, 0, 'Not OAuth user');
           assert.strictEqual(newNoob.name, 'mike wazowski', 'New noob name.');
           assert.strictEqual(newNoob.user_group, 'noob', 'New noob user group.');
           done();
