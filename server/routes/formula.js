d48a256f814f0c587afdd763f02cff68b695aaf0
Christine Zhou <zhou.l.chistine@gmail.com>
2018-02-14 01:08:53 -0500

    Adding tests and for view formulas.

--- server/routes/formula.js
+++ server/routes/formula.js
@@ -2,7 +2,7 @@ import { createError, handleError } from './common/customError';
 import success from './common/success';
 
 
-const formulaQueryString = 'SELECT Formulas.*';
+const formulaQueryString = 'SELECT * FROM Formulas';
 const formulaEntryQuery = 'SELECT FormulaEntries.*';
 
 /**
@@ -32,11 +32,12 @@ export function view(req, res, next) {
                 'id': x.id,
                 'name': x.name,
                 'description': x.description,
+                'num_product': x.num_product,
                 'ingredients': {},
             };
             myFormulas[`${x.id}`] = formulaObject;
         });
-        return connection.query(`${formulaEntryQuery}, Ingredients.name FROM FormulaEntries
+        return connection.query(`${formulaEntryQuery}, Ingredients.name, Ingredients.native_unit FROM FormulaEntries
             JOIN Ingredients ON FormulaEntries.ingredient_id = Ingredients.id`);
     })
     .then((formulaEntries) => {
@@ -44,10 +45,17 @@ export function view(req, res, next) {
             let ingredientTuple = {
                 'quantity': x.quantity,
                 'num_native_units': x.num_native_units,
+                'native_unit': x.native_unit,
             };
-            myFormulas[`${x['formula_id']}`]['ingredients'][`${x.name}`] = ingredientTuple;
+            let currentFormula = myFormulas[`${x['formula_id']}`];
+            if (!('ingredients' in currentFormula)) {
+                currentFormula['ingredients'] = {};
+                currentFormula['ingredients'][`${x.name}`] = ingredientTuple;
+            } else {
+                myFormulas[`${x['formula_id']}`]['ingredients'][`${x.name}`] = ingredientTuple;
+            }
         });
-        res.status.send(myFormulas);
+        res.status(200).send(Object.values(myFormulas));
     })
     .catch((err) => {
         handleError(err, res);
--- test/formula.js
+++ test/formula.js
@@ -4,19 +4,28 @@ const alasql = require('alasql');
 const assert = require('chai').assert;
 const testTokens = require('./testTokens');
 
-// describe('Formulas', () => {
-//     describe('#view()', () => {
-//       it('should return all formulas', (done) => {
-//         chai.request(server)
-//           .get('/formulas')
-//           .set('Authorization', `Token ${testTokens.adminTestToken}`)
-//           .end((err, res) => {
-//             res.should.have.status(200);
-//             res.body.should.be.a('array');
-//             console.log(res.body);
-//             res.body.length.should.be.eql(2);
-//             done();
-//           });
-//       });
-//     });
-// });
+describe('Formulas', () => {
+    describe('#view()', () => {
+      it('should return all formulas', (done) => {
+        chai.request(server)
+          .get('/formulas')
+          .set('Authorization', `Token ${testTokens.adminTestToken}`)
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('array');
+            res.body.length.should.be.eql(2);
+            done();
+          });
+      });
+
+      it('should reject noobs to view', (done) => {
+        chai.request(server)
+          .get('/formulas')
+          .set('Authorization', `Token ${testTokens.noobTestToken}`)
+          .end((err, res) => {
+            res.should.have.status(401);
+            done();
+          });
+      });
+    });
+});
