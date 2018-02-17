1a2dd9ae364fec6fad8f783179a497f0776871a0
Bill Yu <han.yu@duke.edu>
2018-02-17 00:34:07 -0500

    new pagebar

--- frontend/react-client/src/components/GeneralComponents/PageBar.js
+++ frontend/react-client/src/components/GeneralComponents/PageBar.js
@@ -3,23 +3,67 @@ import React, { Component } from 'react';
 class PageBar extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      currentPage: 1,
+    };
     this.onClickLink = this.onClickLink.bind(this);
+    this.onClickPrevious = this.onClickPrevious.bind(this);
+    this.onClickNext = this.onClickNext.bind(this);
   }
 
   onClickLink(idx) {
     this.props.selectPage(idx);
+    this.setState({
+      currentPage: idx,
+    });
+  }
+
+  onClickPrevious() {
+    this.props.selectPage(this.state.currentPage - 1);
+    this.setState({
+      currentPage: this.state.currentPage - 1,
+    });
+  }
+
+  onClickNext() {
+    this.props.selectPage(this.state.currentPage + 1);
+    this.setState({
+      currentPage: this.state.currentPage + 1,
+    });
   }
 
   render() {
     const links = [];
     for (let i = 0; i < this.props.pages; i++) {
-      links.push(<a key={i} className="PageLink" onClick={e => this.onClickLink(i + 1)} href="javascript:void(0)">{i+1}</a>);
+      links.push(i+1);
     }
+    const disablePrev = this.state.currentPage == 1 ? "disabled" : "";
+    const disableNext = this.state.currentPage == this.props.pages ? "disabled" : "";
     return(
-      <div className="PageBar">
-        <p>Pages:</p>
-        {links}
-      </div>
+      <nav aria-label="...">
+        <ul className="pagination justify-content-center">
+          <li className={"page-item " + disablePrev}>
+            <span className="page-link" href="javascript:void(0)" onClick={this.onClickPrevious}>Previous</span>
+          </li>
+          {
+            links.map((pageNum, idx) => {
+              const active = pageNum == this.state.currentPage ? "active" : "";
+              return (
+                <li key={idx} className={"page-item " + active}>
+                  <span className="page-link" onClick={e => this.onClickLink(pageNum)}>
+                    {pageNum}
+                    {active && <span className="sr-only">(current)</span>}
+                  </span>
+                </li>
+              );
+              
+            })
+          }
+          <li className={"page-item " + disableNext}>
+            <a className="page-link" href="javascript:void(0)" onClick={this.onClickNext}>Next</a>
+          </li>
+        </ul>
+      </nav>
     );
   }
 }
