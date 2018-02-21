129cb5543a4b4ca249592de9267975a4dae08625
Bill Yu <han.yu@duke.edu>
2018-02-20 19:07:39 -0500

    formula list

---
+++ frontend/react-client/src/components/homepage/formula/FormulaList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FormulaList extends Component {
+  render() {
+    return(
+      <div>
+        <h2>Formulas</h2>
+      </div>
+    );
+  }
+}
+
+export default FormulaList;
