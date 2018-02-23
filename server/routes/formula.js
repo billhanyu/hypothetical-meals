47cf664cf9d37402c00af335a6d5ec844259829f
Brian Zhou <brian.zhou@duke.edu>
2018-02-22 20:10:59 -0500

    Fix Tests

--- server/routes/formula.js
+++ server/routes/formula.js
@@ -251,7 +251,7 @@ export function modify(req, res, next) {
  * @param {*} next
  */
 export function deleteFormulas(req, res, next) {
-    const toDelete = [...req.headers.formulaid];
+    const toDelete = req.headers.formulaid.split(", ");
     connection.query(`${formulaQueryString} WHERE id IN (${toDelete.join(', ')})`)
         .then((results) => {
             if (results.length != toDelete.length) {
