534cae612136944ee3014ac6e603656a7654fa30
Bill Yu <han.yu@duke.edu>
2018-02-25 20:32:55 -0500

    visuals

--- frontend/react-client/src/components/homepage/inventory/ViewInventory.js
+++ frontend/react-client/src/components/homepage/inventory/ViewInventory.js
@@ -168,6 +168,7 @@ class ViewInventory extends Component {
   }
 
   render() {
+    const columnClass = global.user_group == "admin" ? "OneFifthWidth" : "OneFourthWidth";
     return (
       <div>
         <h2>Inventory</h2>
@@ -179,13 +180,13 @@ class ViewInventory extends Component {
         <table className="table">
           <thead>
             <tr>
-              <th>Ingredient Name</th>
-              <th>Temperature State</th>
-              <th>Package Type</th>
-              <th>Quantity</th>
+              <th className={columnClass}>Ingredient Name</th>
+              <th className={columnClass}>Temperature State</th>
+              <th className={columnClass}>Package Type</th>
+              <th className={columnClass}>Quantity</th>
               {
                 global.user_group == "admin" &&
-                <th>Options</th>
+                <th className={columnClass}>Options</th>
               }
             </tr>
           </thead>
