2155b07473decfdbbc5d02da910d8cddbecf9b0d
Bill Yu <han.yu@duke.edu>
2018-03-06 14:46:55 -0500

    progress

---
+++ frontend/react-client/src/components/homepage/productionrun/ProductionRun.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class ProductionRun extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Production Runs</h3>
+        
+      </div>
+    );
+  }
+}
+
+export default ProductionRun;
