7dad17a516c83c2825dd6137281b9b1a5a1f24d9
Christine Zhou <zhou.l.chistine@gmail.com>
2018-03-10 23:58:13 -0500

    Untested recall report function.

---
+++ test/recallReport.js
@@ -0,0 +1,11 @@
+const assert = require('chai').assert;
+const alasql = require('alasql');
+const testTokens = require('./testTokens');
+
+describe('RecallReport', () => {
+  describe('#getRecallForIngredient()', () => {
+    xit('should return all products related to that recall', (done) => {
+      done();
+    });
+  });
+});
