9671d2e89d6b512caab066f5dee56510353aaac8
Eric Song <ess42@duke.edu>
2018-03-27 02:12:37 -0400

    i could be on discovery channel with all these bugs i have

--- server/routes/inventory.js
+++ server/routes/inventory.js
@@ -47,6 +47,7 @@ function getStockPromise(ids) {
             stock[result.ingredient_id].num_packages += result.num_packages;
           } else {
             // Clone
+            console.log(result);
             stock[result.ingredient_id] = JSON.parse(JSON.stringify(result));
             delete stock[result.ingredient_id].worst_duration;
             delete stock[result.ingredient_id].total_weighted_duration;
@@ -59,6 +60,9 @@ function getStockPromise(ids) {
             inventoryId: result.id,
             numPackages: result.num_packages,
             createdAt: result.created_at,
+
+            lot: result.lot,
+            vendorId: result.vendor_id,
           });
         }
         resolve(stock);
@@ -222,9 +226,9 @@ export function commitCart(req, res, next) {
           });
           productRunEntries.push({
             ingredient_id: ingredientId,
-            vendor_id: inventory.vendor_id,
-            lot: inventory.lot,
-            num_native_units: inventory.ingredient_num_native_units,
+            vendor_id: freshnessEntry.vendorId,
+            lot: freshnessEntry.lot,
+            num_native_units: numPackagesConsumed * inventory.ingredient_num_native_units,
           });
         }
         // Dummy data 'poop', 'sack', 1, 'pounds', 10
