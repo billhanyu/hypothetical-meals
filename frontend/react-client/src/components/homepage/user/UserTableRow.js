6e31f54fd728dc5fe6fe30aba5da662a536a38d7
Bill Yu <han.yu@duke.edu>
2018-03-31 03:06:30 -0400

    delete user fix

--- frontend/react-client/src/components/homepage/user/UserTableRow.js
+++ frontend/react-client/src/components/homepage/user/UserTableRow.js
@@ -1,13 +1,8 @@
 import React, { Component } from 'react';
-import Dialog from 'material-ui/Dialog';
-import FlatButton from 'material-ui/FlatButton';
 
 class UserTableRow extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      open: false,
-    };
   }
 
   /*** REQUIRED PROPS
@@ -33,36 +28,21 @@ class UserTableRow extends Component {
 
   render() {
     const isOAuth = this.props.oauth === 1;
-    const actions = [
-      <FlatButton
-        label="Cancel"
-        primary={true}
-        key={0}
-        onClick={this.handleClose.bind(this)}
-      />,
-      <FlatButton
-        label="Confirm"
-        primary={true}
-        key={1}
-        onClick={() => {this.props.onDelete(this.props.username);}}
-      />,
-    ];
     return (
       <tr>
-        <Dialog
-          title="Confirm delete"
-          actions={actions}
-          modal={false}
-          open={this.state.open}
-          onRequestClose={this.handleClose.bind(this)}
-        >
-          Are you sure you want to delete this user?
-        </Dialog>
         <td>{this.props.name}</td>
         <td>{this.props.username}</td>
         <td style={{ cursor: 'pointer' }} onClick={() => { this.props.changePermission(this.props.username, this.props.user_group, this.props.oauth)}}><i className="far fa-edit"></i> {this._capitalize(this.props.user_group)}</td>
         <td style={{color: isOAuth ? "green" : "red"}}>{isOAuth ? "YES" : "NO"}</td>
-        <td style={{cursor: 'pointer'}} onClick={this.handleOpen.bind(this)}><i className="fas fa-trash"></i></td>
+        <td>
+          <button
+            type='button'
+            onClick={e=>this.props.onDelete(this.props.username)}
+            data-toggle="modal"
+            data-target="#deleteUserModal">
+            <i className="fas fa-trash"></i>
+          </button>
+        </td>
       </tr>
     );
   }
