065cf95b79104f73ac91bc29385df6a27c5610ad
Eric Song <ess42@duke.edu>
2018-04-15 17:08:06 -0400

    Bug fixes

--- server/create_database.sql
+++ server/create_database.sql
@@ -164,6 +164,7 @@ CREATE TABLE ProductRuns(
 	lot VARCHAR(100) not null,
 	cost_for_run double not null,
 	created_at timestamp DEFAULT CURRENT_TIMESTAMP not null,
+	completed BIT default 0,
 
 	FOREIGN KEY (formula_id) REFERENCES Formulas(id),
 	FOREIGN KEY (user_id) REFERENCES Users(id),
