fc6dcfb7c0575aa5f229712d45af5eec60ea9e46
Bill Yu <han.yu@duke.edu>
2018-04-15 17:25:22 -0400

    integrate efficiency report

--- frontend/react-client/src/components/homepage/efficiencyreport/EfficiencyReport.js
+++ frontend/react-client/src/components/homepage/efficiencyreport/EfficiencyReport.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import EfficiencyResult from './EfficiencyResult';
+import axios from 'axios';
+import Snackbar from 'material-ui/Snackbar';
+import qs from 'qs';
 
 class EfficiencyReport extends Component {
   constructor(props) {
     super(props);
     this.state = {
       viewReport: false,
+      result: {
+        occupancies: [],
+        total_lines: 1,
+        total_time: 0,
+      },
       start: '',
       end: '',
+      open: false,
+      message: '',
     };
     this.viewReport = this.viewReport.bind(this);
     this.changeStart = this.changeStart.bind(this);
@@ -15,11 +25,43 @@ class EfficiencyReport extends Component {
     this.back = this.back.bind(this);
   }
 
+  handleRequestClose() {
+    this.setState({
+      open: false,
+    });
+  }
+
   viewReport(e) {
     e.preventDefault();
-    this.setState({
-      viewReport: true,
-    });
+    if (!this.state.start || !this.state.end) {
+      this.setState({
+        open: true,
+        message: 'You have not specified both start end end dates',
+      });
+      return;
+    }
+    axios.get(`/efficiency`, {
+      params: {
+        from_date: this.state.start,
+        to_date: this.state.end,
+      },
+      paramsSerializer: function (params) {
+        return qs.stringify(params, { arrayFormat: 'repeat' });
+      },
+      headers: { Authorization: "Token " + global.token },
+    })
+      .then(response => {
+        this.setState({
+          result: response.data,
+          viewReport: true,
+        });
+      })
+      .catch(err => {
+        this.setState({
+          open: true,
+          message: err.response.data,
+        });
+      });
   }
 
   changeStart(e) {
@@ -44,6 +86,12 @@ class EfficiencyReport extends Component {
     const main =
       <div>
         <h2>Efficiency Report</h2>
+        <Snackbar
+          open={this.state.open}
+          message={this.state.message}
+          autoHideDuration={2500}
+          onRequestClose={this.handleRequestClose.bind(this)}
+        />
         <form>
           <div className="row">
             <div className="col">
@@ -63,7 +111,7 @@ class EfficiencyReport extends Component {
         </form>
       </div>;
     
-    const report = <EfficiencyResult back={this.back} />;
+    const report = <EfficiencyResult back={this.back} result={this.state.result} />;
 
     return this.state.viewReport ? report : main;
   }
