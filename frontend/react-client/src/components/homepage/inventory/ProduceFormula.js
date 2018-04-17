c6d67dfe093652e09ddb82ed881442963de687b5
Brian Zhou <brian.zhou@duke.edu>
2018-04-16 21:29:10 -0400

    Ordering not use pending ingredients

--- frontend/react-client/src/components/homepage/inventory/ProduceFormula.js
+++ frontend/react-client/src/components/homepage/inventory/ProduceFormula.js
@@ -65,13 +65,19 @@ class ProduceFormula extends Component {
       headers: {Authorization: "Token " + global.token},
     })
     .then(response => {
-      console.log(response.data);
-      const inventoryStockArrived = response.data.filter(element => {
-        return element.arrived == 1;
+      const inventoryStock = response.data;
+      Object.keys(response.data).forEach(element => {
+        const data = inventoryStock[element];
+        if(data.arrived == 0) {
+          delete inventoryStock[element];
+        }
       });
+      console.log(response.data);
+      console.log(inventoryStock);
+
       this.setState({
         shouldShowSummaryTable: true,
-        inventoryStock: inventoryStockArrived,
+        inventoryStock,
       });
     })
     .catch(error => {
